8d74614fb6ccb13b6d003a5496455214fc4feaf8
fizzahkhan123 <guysr92@gmail.com>
2023-07-08 03:52:15 -0700

    first commit

---
+++ src/Components/ProductCard.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+export default function () {
+  return (
+    <> 
+       <div className='col-md-3'>
+   <div className="card">
+  <div className="card-body">
+    <h5 className="card-title">Card title</h5>
+    <h6 className="card-subtitle mb-2 text-body-secondary">Card subtitle</h6>
+    <p className="card-text">
+    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nisi minus ex consequuntur mollitia molestias ad, repellendus voluptas veniam! Labore veniam animi minus perferendis beatae pariatur molestias quidem aliquid corrupti porro.
+    </p>
+    <a href="#" className="card-link">
+      Card link
+    </a>
+    <a href="#" className="card-link">
+      Another link
+    </a>
+  </div>
+</div>
+</div>
+    </>
+  )
+}
